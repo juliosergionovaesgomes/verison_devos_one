a385fd7fa45d5ebd0f1bda69fb5db672e3671578
juliosergionovaes <saxnovaesgomes@gmail.com>
2026-02-16 00:01:16 -0300

    Fix: Remove trailing slash from basePath and assetPrefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
+  output: "export",
+  basePath: "/verison-devos-one",
+  assetPrefix: "/verison-devos-one",
   trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
